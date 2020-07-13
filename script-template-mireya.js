86e9d9d8ffc2140c2bb9b53ad22fc2059934ac65
Efren Robles <efren_robles@hotmail.com>
2020-07-13 12:35:03 -0700

    * updating template offset.

--- script-template-mireya.js
+++ script-template-mireya.js
@@ -1,33 +1,37 @@
 $(document).ready(function() {
-    
-    $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
+ 	
+ 	$(window).scroll(function() {    
+	    var scroll = $(window).scrollTop();
 
-        if (scroll >= 569) {
-            $(".navbar").addClass("navbar-fixed-top dark-bar");
-        } else {
-            $(".navbar").removeClass("navbar-fixed-top dark-bar");
-        }
-    });
+	    if (scroll >= 200) {
+	        $(".navbar").addClass("navbar-fixed-top dark-bar");
+	    } else {
+	        $(".navbar").removeClass("navbar-fixed-top dark-bar");
+	    }
+	});
 
-    // Smooth Scroll
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-            || location.hostname == this.hostname) {
 
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-               if (target.length) {
-                 $('html,body').animate({
-                     scrollTop: (target.offset().top - 452)
-                }, 1000);
-                return false;
-            }
-        }
-    });
+	// Smooth Scroll
 
-    // Popup
-    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		$('a[href*="#"]:not([href="#"])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+		        || location.hostname == this.hostname) {
+
+		        var target = $(this.hash);
+		        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		           if (target.length) {
+		             $('html,body').animate({
+		                 scrollTop: (target.offset().top - 330)
+		            }, 1000);
+		            return false;
+		        }
+		    }
+		});
+
+
+ 	// Popup
+
+ 	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
         disableOn: 700,
         type: 'iframe',
         mainClass: 'mfp-fade',
@@ -36,44 +40,49 @@ $(document).ready(function() {
         fixedContentPos: false
     });
 
-    // Slider
-    $('#workstation-slider').owlCarousel({
-        loop:true,
-        margin:30,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-        }
-    })
 
-    $('#expert-slider').owlCarousel({
-        loop:true,
-        items: 1
-    })
 
-    // height
-    var h = $('.expert').height();
-    $('.expert .col-sm-6 div').height(function (index, height) {
-        return (h);
-    });
+ 	// Slider
 
-    // Menu bar
-    $( ".menu" ).click(function() {
-        $(this).toggleClass('m c');
-        $('.menu span').toggleClass('ion-navicon ion-android-close');
-        $('#menu-item').toggleClass( "show-menu hide-menu" );
-    });
+ 	$('#workstation-slider').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:2
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+	})
+	$('#expert-slider').owlCarousel({
+	    loop:true,
+	    items: 1
+	})
 
-    $( "#menu-item a" ).click(function() {
-        $('.menu').toggleClass('c m');
-        $('.menu span').toggleClass('ion-navicon ion-android-close');
-        $('#menu-item').toggleClass( "show-menu hide-menu" );
-    });
+ 	// height
+
+ 	var h = $('.expert').height();
+ 	$('.expert .col-sm-6 div').height(function (index, height) {
+	    return (h);
+	});
+
+ 	// Menu bar
+	$( ".menu" ).click(function() {
+		$(this).toggleClass('m c');
+		$('.menu span').toggleClass('ion-navicon ion-android-close');
+		$('#menu-item').toggleClass( "show-menu hide-menu" );
+	});
+
+	$( "#menu-item a" ).click(function() {
+		$('.menu').toggleClass('c m');
+		$('.menu span').toggleClass('ion-navicon ion-android-close');
+		$('#menu-item').toggleClass( "show-menu hide-menu" );
+	});
+
+
 });
